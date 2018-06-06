5d0afd678a417365a1a732efcf5b1468b60b0453
Jay Vana <jsvana@mtu.edu>
2018-06-06 07:53:58 -0700

    Better modularize

--- base16.py
+++ base16.py
@@ -8,12 +8,9 @@ import sys
 import time
 from collections import namedtuple
 from pathlib import Path
-from typing import Any
+from typing import Any, Callable, Optional
 
 import requests
-
-MAGIC_STRING = "Written by base16 manager, do not modify manually"
-SUPPORTED_PLUGINS = ["xresources", "dunst", "i3"]
 
 
 class PathNotFoundError(Exception):
@@ -69,89 +66,169 @@ def parse_args() -> argparse.Namespace:
     return parser.parse_args()
 
 
-def sync_xresources(config: Config) -> bool:
-    proc = subprocess.run(["xrdb", "-merge", config.path])
+class PluginInfo:
+    def __init__(
+        self,
+        name: str,
+        comment: str,
+        path_in_home: Path,
+        install_func: Optional[Callable[["PluginInfo", str], bool]],
+    ) -> None:
+        self.name = name
+        self.comment = comment
+        self.path = Path.home() / path_in_home
+        self.install_func = install_func
+
+    def install(self) -> bool:
+        if self.install_func is None:
+            return True
+        return self.install_func()
+
+
+class DownloadedPluginInfo(PluginInfo):
+
+    MAGIC_STRING = "Written by base16 manager, do not modify manually"
+
+    def __init__(
+        self,
+        name: str,
+        comment: str,
+        path_in_home: Path,
+        theme_url: str,
+        post_process_func: Optional[Callable[["PluginInfo", str], bool]] = None,
+    ) -> None:
+        super().__init__(name, comment, path_in_home, None)
+        self.theme_url = theme_url
+        self.post_process_func = post_process_func
+
+    def install(self, theme: str) -> bool:
+        try:
+            theme_str = self.get_file(theme)
+        except PathNotFoundError:
+            print(f"Unable to fetch {self.name} theme for {theme}", file=sys.stderr)
+            return False
+
+        if not self.generate(theme_str):
+            return False
+
+        return True
+
+    def validate(self) -> bool:
+        base_path = self.path.parent / (self.path.name + ".base")
+        if not self.path.is_file():
+            if not base_path.is_file():
+                print(
+                    "No {self.name} or {self.name}.base file exist. Please create "
+                    f"a base configuration at {base_path}. base16 take that and "
+                    "concatenate it with the downloaded theme file.",
+                    file=sys.stderr,
+                )
+                return False
+            return True
+
+        with self.path.open() as f:
+            lines = f.readlines()
+
+        if not lines:
+            return True
+
+        if len(lines) < 2:
+            print(f'"{self.path}" has an invalid header', file=sys.stderr)
+            return False
+
+        if lines[0].strip() != f"{self.comment} {self.MAGIC_STRING}":
+            print(
+                f'"{self.path}" does not appear to be managed by base16. Move your '
+                f'existing {self.path.name} file to "{self.path.name}.base" and re-run, and '
+                "base16 will concatenate that file with the downloaded theme file",
+                file=sys.stderr,
+            )
+            return False
+
+        m = re.match(f"{self.comment} Generated (\d+)$", lines[1].strip())
+        if m is None:
+            print(f'Invalid timestamp in header of "{self.path}"', file=sys.stderr)
+            return False
+
+        if abs(int(os.path.getmtime(self.path)) - int(m.group(1))) > 5:
+            print(
+                f'"{self.path}" appears to have been modified after generation. '
+                f"Please make your changes in {self.path.name}.base instead and regenerate "
+                "the configuration file.",
+                file=sys.stderr,
+            )
+            return False
+
+        return True
+
+    def get_file(self, theme: str) -> str:
+        response = requests.get(self.theme_url.format(theme))
+        if response.status_code == 404:
+            raise PathNotFoundError(f"{path} is not a valid file")
+        return response.text
+
+    def generate(self, theme_str: str) -> bool:
+        if not self.validate():
+            return False
+
+        output = f"{self.comment} {self.MAGIC_STRING}\n{self.comment} Generated {int(time.time())}\n"
+        base_path = self.path.parent / (self.path.name + ".base")
+        try:
+            with base_path.open() as f:
+                output += f.read()
+        except FileNotFoundError:
+            print(
+                "No {self.path.name}.base file found. Run `base16 doctor` for help.",
+                file=sys.stderr,
+            )
+            return False
+
+        if not output.endswith("\n"):
+            output += "\n"
+
+        output += theme_str
+
+        path = self.path
+        with path.open("w") as f:
+            f.write(output)
+
+        if self.post_process_func is not None and not self.post_process_func(self):
+            return False
+
+        print(f"{self.name} updated successfully")
+
+        return True
+
+
+def sync_xresources(plugin_info: DownloadedPluginInfo) -> bool:
+    proc = subprocess.run(["xrdb", "-merge", plugin_info.path])
     if proc.returncode != 0:
         print("Error running xrdb", file=sys.stderr)
         return False
     return True
 
 
-ConfigInfo = namedtuple("ConfigInfo", ["comment", "path", "theme_url", "post_process"])
-CONFIG_INFO = {
-    "xresources": ConfigInfo(
+SUPPORTED_PLUGINS = {
+    "xresources": DownloadedPluginInfo(
+        "xresources",
         "!",
-        Path.home() / ".Xresources",
+        Path(".Xresources"),
         "https://raw.githubusercontent.com/chriskempson/base16-xresources/master/xresources/base16-{}.Xresources",
-        sync_xresources,
+        post_process_func=sync_xresources,
     ),
-    "dunst": ConfigInfo(
+    "dunst": DownloadedPluginInfo(
+        "dunst",
         "#",
-        Path.home() / ".config/dunst/dunstrc",
+        Path(".config/dunst/dunstrc"),
         "https://raw.githubusercontent.com/khamer/base16-dunst/master/themes/base16-{}.dunstrc",
-        None,
     ),
-    "i3": ConfigInfo(
+    "i3": DownloadedPluginInfo(
+        "i3",
         "#",
-        Path.home() / ".config/i3/config",
+        Path(".config/i3/config"),
         "https://raw.githubusercontent.com/khamer/base16-i3/master/colors/base16-{}.config",
-        None,
     ),
 }
-
-
-def validate_config(config_name: str) -> bool:
-    config = CONFIG_INFO.get(config_name)
-    if config is None:
-        print(f"{config_name} is not a supported configuration file", file=sys.stderr)
-        return False
-
-    base_path = config.path.parent / (config.path.name + ".base")
-    if not config.path.is_file():
-        if not base_path.is_file():
-            print(
-                "No {config_name} or {config_name}.base file exist. Please create "
-                f"a base configuration at {base_path}. base16 take that and "
-                "concatenate it with the downloaded theme file.",
-                file=sys.stderr,
-            )
-            return False
-        return True
-
-    with config.path.open() as f:
-        lines = f.readlines()
-
-    if not lines:
-        return True
-
-    if len(lines) < 2:
-        print(f'"{config.path}" has an invalid header', file=sys.stderr)
-        return False
-
-    if lines[0].strip() != f"{config.comment} {MAGIC_STRING}":
-        print(
-            f'"{config.path}" does not appear to be managed by base16. Move your '
-            f'existing {config.path.name} file to "{config.path.name}.base" and re-run, and '
-            "base16 will concatenate that file with the downloaded theme file",
-            file=sys.stderr,
-        )
-        return False
-
-    m = re.match(f"{config.comment} Generated (\d+)$", lines[1].strip())
-    if m is None:
-        print(f'Invalid timestamp in header of "{config.path}"', file=sys.stderr)
-        return False
-
-    if abs(int(os.path.getmtime(config.path)) - int(m.group(1))) > 5:
-        print(
-            f'"{config.path}" appears to have been modified after generation. '
-            f"Please make your changes in {config.path.name}.base instead and regenerate "
-            "the configuration file.",
-            file=sys.stderr,
-        )
-        return False
-
-    return True
 
 
 def cmd_doctor(args: argparse.Namespace) -> int:
@@ -161,7 +238,7 @@ def cmd_doctor(args: argparse.Namespace) -> int:
             f.write("{}\n")
 
     config = Config(args.config_path)
-    unsupported_plugins = set(config.enabled) - SUPPORTED_PLUGINS
+    unsupported_plugins = set(config.enabled) - set(SUPPORTED_PLUGINS)
     if unsupported_plugins:
         print(
             "Unsupported plugin(s) enabled: {}".format(
@@ -170,74 +247,24 @@ def cmd_doctor(args: argparse.Namespace) -> int:
         )
         return 1
 
-    for plugin in SUPPORTED_PLUGINS:
+    for plugin, plugin_info in SUPPORTED_PLUGINS.items():
         if plugin not in config.enabled:
             continue
 
-        if not validate_config(plugin):
+        if not plugin_info.validate():
             return 1
 
     print("All set to manage Base16 themes!")
     return 0
 
 
-def get_file(path: str) -> str:
-    response = requests.get(path)
-    if response.status_code == 404:
-        raise PathNotFoundError(f"{path} is not a valid file")
-    return response.text
-
-
-def generate_config(config_name: str, theme_str: str) -> bool:
-    config = CONFIG_INFO.get(config_name)
-    if config is None:
-        print(f"{config_name} is not a supported configuration file", file=sys.stderr)
-        return False
-
-    if not validate_config(config_name):
-        return False
-
-    output = f"{config.comment} {MAGIC_STRING}\n{config.comment} Generated {int(time.time())}\n"
-    base_path = config.path.parent / (config.path.name + ".base")
-    try:
-        with base_path.open() as f:
-            output += f.read()
-    except FileNotFoundError:
-        print(
-            "No {config.path.name}.base file found. Run `base16 doctor` for help.",
-            file=sys.stderr,
-        )
-        return False
-
-    if not output.endswith("\n"):
-        output += "\n"
-
-    output += theme_str
-
-    path = config.path
-    with path.open("w") as f:
-        f.write(output)
-
-    if config.post_process is not None and not config.post_process(config):
-        return False
-
-    print(f"{config_name} updated successfully")
-
-    return True
-
-
 def cmd_install(args: argparse.Namespace, config: Config) -> int:
-    for plugin in SUPPORTED_PLUGINS:
+    for plugin, plugin_info in SUPPORTED_PLUGINS.items():
         if plugin not in config.enabled:
             continue
 
-        try:
-            theme_str = get_file(CONFIG_INFO[plugin].theme_url.format(args.theme))
-        except PathNotFoundError:
-            print(f"Unable to fetch {plugin} theme for {args.theme}", file=sys.stderr)
-            return 1
-
-        if not generate_config(plugin, theme_str):
+        config_info = SUPPORTED_PLUGINS[plugin]
+        if not config_info.install(args.theme):
             return 1
 
     # TODO(jsvana): somehow run zsh functions in parent. maybe need to just instruct user to reopen shell
